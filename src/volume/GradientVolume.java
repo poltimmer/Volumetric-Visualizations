3a967934f7473897a17f1553aa08164206a2cc98
Buskebam <th.buskens@gmail.com>
2020-11-12 10:11:38 +0100

    Fixed null pointer in gradient computation (TODO 4)

--- src/volume/GradientVolume.java
+++ src/volume/GradientVolume.java
@@ -59,12 +59,13 @@ public class GradientVolume {
         for(int i=1; i< dimX-1;i++){
             for(int j=1; j< dimX-1;j++){
                 for(int k=1; k< dimX-1;k++){
-                    VoxelGradient currentGradient = getGradient(i,j,k);
-                    //use 0.5 as float, double precision is not saved
-                    currentGradient.x = (float) 0.5*(volume.getVoxel(i+1,j,k)-volume.getVoxel(i-1,j,k));
-                    currentGradient.y = (float) 0.5*(volume.getVoxel(i,j+1,k)-volume.getVoxel(i,j-1,k));
-                    currentGradient.z = (float) 0.5*(volume.getVoxel(i,j,k+1)-volume.getVoxel(i,j,k-1));
                     
+                    //use 0.5 as float, double precision is not saved
+                    float x = (float) 0.5*(volume.getVoxel(i+1,j,k)-volume.getVoxel(i-1,j,k));
+                    float y = (float) 0.5*(volume.getVoxel(i,j+1,k)-volume.getVoxel(i,j-1,k));
+                    float z = (float) 0.5*(volume.getVoxel(i,j,k+1)-volume.getVoxel(i,j,k-1));
+                    
+                    setGradient(i,j,k,new VoxelGradient(x,y,z));
                 }
             }
         }
